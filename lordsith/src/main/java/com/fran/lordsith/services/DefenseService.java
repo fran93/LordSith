facb68510254168caee93b76cf5e1428051e75b3
fran <fran@fran-pc>
2019-12-29 12:34:05 +0100

    Check queue.

--- lordsith/src/main/java/com/fran/lordsith/services/DefenseService.java
+++ lordsith/src/main/java/com/fran/lordsith/services/DefenseService.java
@@ -14,102 +14,112 @@ import com.fran.lordsith.enums.TechnologyEnum;
 
 @Service
 public class DefenseService {
-	
-	private static final String LI_DATA_TECHNOLOGY = "//li[@data-technology=";
-	private static final int BASE_RAKETENWERFER = 1920;
-	private static final int BASE_LEICHTESLASERGESCHUTZ = 1920;
-	private static final int BASE_SCHWERESLASERGESCHUTZ = 192;
-	private static final int BASE_IONENGESCHUZ = 192;
-	private static final int BASE_GAUSSKANONE = 96;
-	private static final int BASE_PLASMAWERFER = 48;
-	
-	@Autowired @Lazy
-	private FirefoxClient firefox;
-	
-	Logger log = LoggerFactory.getLogger(DefenseService.class);
 
-	public long calculateNumberOfDefense(int baseNumber, long points) {
-		if(points < 50000) {
-			return 0;
-		} else if(points < 500000) {
-			return (long) (baseNumber * 0.25);
-		} else if(points < 1000000) {
-			return (long) (baseNumber * 0.50);
-		} else {
-			return (points/1000000) * baseNumber;
-		}
-	}
-	
-	public void buildDefenses(int position, long points) throws InterruptedException {	
-		firefox.get().findElements(By.className("menubutton")).get(MenuEnum.VERTEIDIGUNG.getId()).click();
-		firefox.shortLoading();
-		
-		int adjust = position == 0 ? 1 : 10;
-		long desiredRaketenwerfer = calculateNumberOfDefense(BASE_RAKETENWERFER, points)/adjust;
-		long desiredLeichtesLaser = calculateNumberOfDefense(BASE_LEICHTESLASERGESCHUTZ, points)/adjust;
-		long desiredSchweresLaser = calculateNumberOfDefense(BASE_SCHWERESLASERGESCHUTZ, points)/adjust;
-		long desiredIonenGeschuz = calculateNumberOfDefense(BASE_IONENGESCHUZ, points)/adjust;
-		long desiredGaussKanone = calculateNumberOfDefense(BASE_GAUSSKANONE, points)/adjust;
-		long desiredPlasmaWerfer = calculateNumberOfDefense(BASE_PLASMAWERFER, points)/adjust;
-		
-		long amountRaketenwerfer = getAmount(TechnologyEnum.RAKETENWERFER.getId());
-		long amountLeichtesLaser  = getAmount(TechnologyEnum.LEICHTESLASERGESCHUTZ.getId());
-		long amountSchweresLaser  = getAmount(TechnologyEnum.SCHWERESLASERGESCHUTZ.getId());
-		long amountIonenGeschuz= getAmount(TechnologyEnum.IONENGESCHUZ.getId());
-		long amountGaussKanone = getAmount(TechnologyEnum.GAUSSKANONE.getId());
-		long amountPlasmaWerfer = getAmount(TechnologyEnum.PLASMAWERFER.getId());
-		
-		if(isStatusOn(TechnologyEnum.RAKETENWERFER.getId()) && desiredRaketenwerfer > amountRaketenwerfer) {
-		    build(TechnologyEnum.RAKETENWERFER, desiredRaketenwerfer - amountRaketenwerfer);
-		} else if(isStatusOn(TechnologyEnum.LEICHTESLASERGESCHUTZ.getId()) && desiredLeichtesLaser > amountLeichtesLaser) {
-		    build(TechnologyEnum.LEICHTESLASERGESCHUTZ, desiredLeichtesLaser - amountLeichtesLaser);
-		} else if(isStatusOn(TechnologyEnum.SCHWERESLASERGESCHUTZ.getId()) && desiredSchweresLaser > amountSchweresLaser) {
-		    build(TechnologyEnum.SCHWERESLASERGESCHUTZ, desiredSchweresLaser - amountSchweresLaser);
-		} else if(isStatusOn(TechnologyEnum.IONENGESCHUZ.getId()) && desiredIonenGeschuz > amountIonenGeschuz) {
-		    build(TechnologyEnum.IONENGESCHUZ, desiredIonenGeschuz - amountIonenGeschuz);
-		} else if(isStatusOn(TechnologyEnum.GAUSSKANONE.getId()) && desiredGaussKanone > amountGaussKanone) {
-		    build(TechnologyEnum.GAUSSKANONE, desiredGaussKanone - amountGaussKanone);
-		} else if(isStatusOn(TechnologyEnum.PLASMAWERFER.getId()) && desiredPlasmaWerfer > amountPlasmaWerfer) {
-		    build(TechnologyEnum.PLASMAWERFER, desiredPlasmaWerfer - amountPlasmaWerfer);
-		} else if(isStatusOn(TechnologyEnum.KLEINE_SCHILDKUPPEL.getId())) {
-		    build(TechnologyEnum.KLEINE_SCHILDKUPPEL);
-		} else if(isStatusOn(TechnologyEnum.GROSSE_SCHILDKUPPEL.getId())) {
-		    build(TechnologyEnum.GROSSE_SCHILDKUPPEL);
-		} else if(isStatusOn(TechnologyEnum.ABFANGRAKETE.getId())) {
-		    build(TechnologyEnum.ABFANGRAKETE, 5);
-		}
-		
-	}
-	
-	private long getAmount(int id) {
-		return Long.parseLong(firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY+id+"]")).findElement(By.className("amount")).getAttribute("data-value"));
-	}
-	
-	private boolean isStatusOn(int id) {
-		WebElement defense  = firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY+id+"]"));
-		return defense.getAttribute("data-status").equals(StatusEnum.ON.getValue()) && defense.findElements(By.className("targetamount")).isEmpty();
-	}
-	
-	private void build(TechnologyEnum defense) throws InterruptedException {
-		firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY+defense.getId()+"]")).click();
-		firefox.shortLoading();
+    private static final String LI_DATA_TECHNOLOGY = "//li[@data-technology=";
+    private static final int BASE_RAKETENWERFER = 1920;
+    private static final int BASE_LEICHTESLASERGESCHUTZ = 1920;
+    private static final int BASE_SCHWERESLASERGESCHUTZ = 192;
+    private static final int BASE_IONENGESCHUZ = 192;
+    private static final int BASE_GAUSSKANONE = 96;
+    private static final int BASE_PLASMAWERFER = 48;
 
-		firefox.jsClick(firefox.get().findElement(By.className("upgrade")));
-		firefox.shortLoading();
-		
-		log.info("I order to build " + defense.name());
+    @Autowired
+    @Lazy
+    private FirefoxClient firefox;
+
+    Logger log = LoggerFactory.getLogger(DefenseService.class);
+
+    public long calculateNumberOfDefense(int baseNumber, long points) {
+	if (points < 50000) {
+	    return 0;
+	} else if (points < 500000) {
+	    return (long) (baseNumber * 0.25);
+	} else if (points < 1000000) {
+	    return (long) (baseNumber * 0.50);
+	} else {
+	    return (points / 1000000) * baseNumber;
 	}
+    }
+
+    public void buildDefenses(int position, long points) throws InterruptedException {
+	firefox.get().findElements(By.className("menubutton")).get(MenuEnum.VERTEIDIGUNG.getId()).click();
+	firefox.shortLoading();
+
+	int adjust = position == 0 ? 1 : 10;
+	long desiredRaketenwerfer = calculateNumberOfDefense(BASE_RAKETENWERFER, points) / adjust;
+	long desiredLeichtesLaser = calculateNumberOfDefense(BASE_LEICHTESLASERGESCHUTZ, points) / adjust;
+	long desiredSchweresLaser = calculateNumberOfDefense(BASE_SCHWERESLASERGESCHUTZ, points) / adjust;
+	long desiredIonenGeschuz = calculateNumberOfDefense(BASE_IONENGESCHUZ, points) / adjust;
+	long desiredGaussKanone = calculateNumberOfDefense(BASE_GAUSSKANONE, points) / adjust;
+	long desiredPlasmaWerfer = calculateNumberOfDefense(BASE_PLASMAWERFER, points) / adjust;
+
+	long amountRaketenwerfer = getAmount(TechnologyEnum.RAKETENWERFER.getId());
+	long amountLeichtesLaser = getAmount(TechnologyEnum.LEICHTESLASERGESCHUTZ.getId());
+	long amountSchweresLaser = getAmount(TechnologyEnum.SCHWERESLASERGESCHUTZ.getId());
+	long amountIonenGeschuz = getAmount(TechnologyEnum.IONENGESCHUZ.getId());
+	long amountGaussKanone = getAmount(TechnologyEnum.GAUSSKANONE.getId());
+	long amountPlasmaWerfer = getAmount(TechnologyEnum.PLASMAWERFER.getId());
+
+	if (isStatusOn(TechnologyEnum.RAKETENWERFER.getId()) && desiredRaketenwerfer > amountRaketenwerfer) {
+	    build(TechnologyEnum.RAKETENWERFER, desiredRaketenwerfer - amountRaketenwerfer);
+	} else if (isStatusOn(TechnologyEnum.LEICHTESLASERGESCHUTZ.getId())
+		&& desiredLeichtesLaser > amountLeichtesLaser) {
+	    build(TechnologyEnum.LEICHTESLASERGESCHUTZ, desiredLeichtesLaser - amountLeichtesLaser);
+	} else if (isStatusOn(TechnologyEnum.SCHWERESLASERGESCHUTZ.getId())
+		&& desiredSchweresLaser > amountSchweresLaser) {
+	    build(TechnologyEnum.SCHWERESLASERGESCHUTZ, desiredSchweresLaser - amountSchweresLaser);
+	} else if (isStatusOn(TechnologyEnum.IONENGESCHUZ.getId()) && desiredIonenGeschuz > amountIonenGeschuz) {
+	    build(TechnologyEnum.IONENGESCHUZ, desiredIonenGeschuz - amountIonenGeschuz);
+	} else if (isStatusOn(TechnologyEnum.GAUSSKANONE.getId()) && desiredGaussKanone > amountGaussKanone) {
+	    build(TechnologyEnum.GAUSSKANONE, desiredGaussKanone - amountGaussKanone);
+	} else if (isStatusOn(TechnologyEnum.PLASMAWERFER.getId()) && desiredPlasmaWerfer > amountPlasmaWerfer) {
+	    build(TechnologyEnum.PLASMAWERFER, desiredPlasmaWerfer - amountPlasmaWerfer);
+	} else if (isStatusOn(TechnologyEnum.KLEINE_SCHILDKUPPEL.getId())) {
+	    build(TechnologyEnum.KLEINE_SCHILDKUPPEL);
+	} else if (isStatusOn(TechnologyEnum.GROSSE_SCHILDKUPPEL.getId())) {
+	    build(TechnologyEnum.GROSSE_SCHILDKUPPEL);
+	} else if (isStatusOn(TechnologyEnum.ABFANGRAKETE.getId())) {
+	    build(TechnologyEnum.ABFANGRAKETE, 5);
+	}
+
+    }
+
+    private long getAmount(int id) {
+	return Long.parseLong(firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY + id + "]"))
+		.findElement(By.className("amount")).getAttribute("data-value"));
+    }
+
+    private boolean isStatusOn(int id) {
+	boolean isInQueue = firefox.get().findElements(By.className("queuePic")).stream().noneMatch(pic -> pic.getAttribute("alt").trim().endsWith("_"+id));
 	
-	private void build(TechnologyEnum defense, long quantity) throws InterruptedException {
-		firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY+defense.getId()+"]")).click();
-		firefox.shortLoading();
-		
-		firefox.get().findElement(By.id("build_amount")).sendKeys(String.valueOf(quantity));
-		firefox.loading();
-		
-		firefox.jsClick(firefox.get().findElement(By.className("upgrade")));
-		firefox.shortLoading();
-		
-		log.info("I order to build " + quantity + " " + defense.name());
+	WebElement defense = firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY + id + "]"));
+	return defense.getAttribute("data-status").equals(StatusEnum.ON.getValue()) && isInQueue;
+    }
+
+    private void build(TechnologyEnum defense) throws InterruptedException {
+	firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY + defense.getId() + "]")).click();
+	firefox.shortLoading();
+
+	if (!firefox.get().findElements(By.className("upgrade")).isEmpty()) {
+	    firefox.jsClick(firefox.get().findElement(By.className("upgrade")));
+	    firefox.shortLoading();
 	}
+
+	log.info("I order to build " + defense.name());
+    }
+
+    private void build(TechnologyEnum defense, long quantity) throws InterruptedException {
+	firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY + defense.getId() + "]")).click();
+	firefox.shortLoading();
+
+	firefox.get().findElement(By.id("build_amount")).sendKeys(String.valueOf(quantity));
+	firefox.loading();
+
+	if (!firefox.get().findElements(By.className("upgrade")).isEmpty()) {
+	    firefox.jsClick(firefox.get().findElement(By.className("upgrade")));
+	    firefox.shortLoading();
+	}
+
+	log.info("I order to build " + quantity + " " + defense.name());
+    }
 }
--- lordsith/src/main/java/com/fran/lordsith/services/HangarService.java
+++ lordsith/src/main/java/com/fran/lordsith/services/HangarService.java
@@ -14,68 +14,75 @@ import com.fran.lordsith.enums.TechnologyEnum;
 
 @Service
 public class HangarService {
-	
-	@Autowired @Lazy
-	private FirefoxClient firefox;
-	
-	@Autowired @Lazy
-	private ManageFleetService expeditionService;
-	
-	private static final String LI_DATA_TECHNOLOGY = "//li[@data-technology=";
-	
-	Logger log = LoggerFactory.getLogger(HangarService.class);
 
-	public void buildExpeditionFleet(long points) throws InterruptedException {
-		firefox.get().findElements(By.className("menubutton")).get(MenuEnum.SCHIFFSWERFT.getId()).click();
-		firefox.shortLoading();
-		
-		int desiredShips = expeditionService.calculateNumberOfCargos(points)/2;
-		long amountCargos = getAmount(TechnologyEnum.GROSSER_TRANSPORTER.getId());
-		long amountPath = getAmount(TechnologyEnum.PATHFINDER.getId());
-		long amountZerstorer = getAmount(TechnologyEnum.ZERSTORER.getId());
-		
-		if(isStatusOn(TechnologyEnum.GROSSER_TRANSPORTER.getId()) && amountCargos < desiredShips) {
-			build(TechnologyEnum.GROSSER_TRANSPORTER, desiredShips - amountCargos);
-		}
-		
-		if(isStatusOn(TechnologyEnum.PATHFINDER.getId()) && amountPath == 0) {
-			build(TechnologyEnum.PATHFINDER, 1);
-		}
-		
-		if(isStatusOn(TechnologyEnum.ZERSTORER.getId()) && amountZerstorer == 0) {
-			build(TechnologyEnum.ZERSTORER, 1);
-		}
+    @Autowired
+    @Lazy
+    private FirefoxClient firefox;
+
+    @Autowired
+    @Lazy
+    private ManageFleetService expeditionService;
+
+    private static final String LI_DATA_TECHNOLOGY = "//li[@data-technology=";
+
+    Logger log = LoggerFactory.getLogger(HangarService.class);
+
+    public void buildExpeditionFleet(long points) throws InterruptedException {
+	firefox.get().findElements(By.className("menubutton")).get(MenuEnum.SCHIFFSWERFT.getId()).click();
+	firefox.shortLoading();
+
+	int desiredShips = expeditionService.calculateNumberOfCargos(points) / 2;
+	long amountCargos = getAmount(TechnologyEnum.GROSSER_TRANSPORTER.getId());
+	long amountPath = getAmount(TechnologyEnum.PATHFINDER.getId());
+	long amountZerstorer = getAmount(TechnologyEnum.ZERSTORER.getId());
+
+	if (isStatusOn(TechnologyEnum.GROSSER_TRANSPORTER.getId()) && amountCargos < desiredShips) {
+	    build(TechnologyEnum.GROSSER_TRANSPORTER, desiredShips - amountCargos);
 	}
-	
-	public void buildPathfinderFleet(long points) throws InterruptedException {
-		int desiredShips = expeditionService.calculateNumberOfCargos(points)/10;
-		long amountPath = getAmount(TechnologyEnum.PATHFINDER.getId());
-		
-		if(isStatusOn(TechnologyEnum.PATHFINDER.getId()) && amountPath < desiredShips) {
-			build(TechnologyEnum.PATHFINDER, desiredShips - amountPath);
-		}
+
+	if (isStatusOn(TechnologyEnum.PATHFINDER.getId()) && amountPath == 0) {
+	    build(TechnologyEnum.PATHFINDER, 1);
 	}
-	
-	private long getAmount(int id) {
-		return Long.parseLong(firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY+id+"]")).findElement(By.className("amount")).getAttribute("data-value"));
+
+	if (isStatusOn(TechnologyEnum.ZERSTORER.getId()) && amountZerstorer == 0) {
+	    build(TechnologyEnum.ZERSTORER, 1);
 	}
-	
-	private boolean isStatusOn(int id) {
-		WebElement ship  = firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY+id+"]"));
-		return ship.getAttribute("data-status").equals(StatusEnum.ON.getValue()) && ship.findElements(By.className("targetamount")).isEmpty();
+    }
+
+    public void buildPathfinderFleet(long points) throws InterruptedException {
+	int desiredShips = expeditionService.calculateNumberOfCargos(points) / 10;
+	long amountPath = getAmount(TechnologyEnum.PATHFINDER.getId());
+
+	if (isStatusOn(TechnologyEnum.PATHFINDER.getId()) && amountPath < desiredShips) {
+	    build(TechnologyEnum.PATHFINDER, desiredShips - amountPath);
 	}
+    }
+
+    private long getAmount(int id) {
+	return Long.parseLong(firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY + id + "]"))
+		.findElement(By.className("amount")).getAttribute("data-value"));
+    }
+
+    private boolean isStatusOn(int id) {
+	boolean isInQueue = firefox.get().findElements(By.className("queuePic")).stream().noneMatch(pic -> pic.getAttribute("alt").trim().endsWith("_"+id));
 	
-	private void build(TechnologyEnum tech, long quantity) throws InterruptedException {
-		firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY+tech.getId()+"]")).click();
-		firefox.shortLoading();
-		
-		firefox.get().findElement(By.id("build_amount")).sendKeys(String.valueOf(quantity));
-		firefox.loading();
-		
-		firefox.jsClick(firefox.get().findElement(By.className("upgrade")));
-		firefox.shortLoading();
-		
-		log.info("I order to build " + quantity + " " + tech.name());
+	WebElement defense = firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY + id + "]"));
+	return defense.getAttribute("data-status").equals(StatusEnum.ON.getValue()) && isInQueue;
+    }
+
+    private void build(TechnologyEnum tech, long quantity) throws InterruptedException {
+	firefox.get().findElement(By.xpath(LI_DATA_TECHNOLOGY + tech.getId() + "]")).click();
+	firefox.shortLoading();
+
+	firefox.get().findElement(By.id("build_amount")).sendKeys(String.valueOf(quantity));
+	firefox.loading();
+
+	if (!firefox.get().findElements(By.className("upgrade")).isEmpty()) {
+	    firefox.jsClick(firefox.get().findElement(By.className("upgrade")));
+	    firefox.shortLoading();
 	}
-	
+
+	log.info("I order to build " + quantity + " " + tech.name());
+    }
+
 }
