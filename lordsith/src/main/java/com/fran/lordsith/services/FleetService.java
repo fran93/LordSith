c168539661a9e2998a0cb07cd2a51a98557a1809
ff93 <cuadradomaldonado@gmail.com>
2020-01-19 10:34:03 +0100

    fixing bugs

--- lordsith/src/main/java/com/fran/lordsith/services/FleetService.java
+++ lordsith/src/main/java/com/fran/lordsith/services/FleetService.java
@@ -2,10 +2,7 @@ package com.fran.lordsith.services;
 
 import com.fran.lordsith.enums.MenuEnum;
 import com.fran.lordsith.enums.StatusEnum;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -101,7 +98,7 @@ public class FleetService {
 
           return currentSlots < maxSlots;
         }
-      } catch (NoSuchElementException ex) {
+      } catch (NoSuchElementException | TimeoutException ex) {
         log.info("isFleetAvailable: " + ex.getMessage());
       }
     }
