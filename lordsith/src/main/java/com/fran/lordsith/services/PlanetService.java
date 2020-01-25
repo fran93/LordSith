bb08eb263f4d80302064d7383a70f8250183770b
ff93 <cuadradomaldonado@gmail.com>
2020-01-25 09:23:06 +0100

    fixing bugs

--- lordsith/src/main/java/com/fran/lordsith/services/PlanetService.java
+++ lordsith/src/main/java/com/fran/lordsith/services/PlanetService.java
@@ -15,99 +15,93 @@ import java.util.Locale;
 @Service
 public class PlanetService {
 
-    @Autowired
-    @Lazy
-    FirefoxClient firefox;
+  private final static int MIN_DEVELOPMENT = 150;
+  @Autowired
+  @Lazy
+  FirefoxClient firefox;
+  @Autowired
+  @Lazy
+  MessageSource messageSource;
+  @Autowired
+  @Lazy
+  MenuService menuService;
+  Logger log = LoggerFactory.getLogger(PlanetService.class);
+  private long points;
+  private int currentFields;
+  private int maxFields;
 
-    @Autowired
-    @Lazy
-    MessageSource messageSource;
-
-    @Autowired
-    @Lazy
-    MenuService menuService;
-
-    private long points;
-
-    private int currentFields;
-    private int maxFields;
-
-    private final static int MIN_DEVELOPMENT = 150;
-
-    Logger log = LoggerFactory.getLogger(PlanetService.class);
-
-    public void extractPoints() {
-      try {
-        firefox.loading(By.id("scoreContentField"));
-        String scoreContentField = firefox.get().findElement(By.id("scoreContentField")).getText();
-        if (!scoreContentField.isEmpty()) {
-          points = Long.parseLong(scoreContentField.split(" ")[0].replaceAll("\\.", ""));
-        }
-      } catch (NoSuchElementException | TimeoutException ex) {
-        log.info("extractPoints: " + ex.getMessage());
+  public void extractPoints() {
+    try {
+      firefox.loading(By.id("scoreContentField"));
+      String scoreContentField = firefox.get().findElement(By.id("scoreContentField")).getText();
+      if (!scoreContentField.isEmpty()) {
+        points = Long.parseLong(scoreContentField.split(" ")[0].replaceAll("\\.", ""));
       }
+    } catch (NoSuchElementException | TimeoutException ex) {
+      log.info("extractPoints: " + ex.getMessage());
     }
+  }
 
-    public void nextPlanet(int index) throws InterruptedException {
-        menuService.openPage(MenuEnum.UBERSICHT);
-        WebElement planet = getPlanetByIndex(index);
-        String name = planet.findElement(By.className("planet-name")).getText();
-      planet.click();
-      firefox.loading(3);
-      extractPoints();
-        extractFreeFields();
-        log.info(messageSource.getMessage("login.points", new Object[]{points}, Locale.ENGLISH));
-        log.info(messageSource.getMessage("planet.fields", new Object[]{name, getFreeFields()}, Locale.ENGLISH));
+  public void nextPlanet(int index) throws InterruptedException {
+    menuService.openPage(MenuEnum.UBERSICHT);
+    WebElement planet = getPlanetByIndex(index);
+    String name = planet.findElement(By.className("planet-name")).getText();
+    firefox.jsClick(planet);
+    firefox.loading(3);
+    extractPoints();
+    extractFreeFields();
+    log.info(messageSource.getMessage("login.points", new Object[]{points}, Locale.ENGLISH));
+    log.info(messageSource.getMessage("planet.fields", new Object[]{name, getFreeFields()}, Locale.ENGLISH));
 
+  }
+
+  public WebElement getPlanetByIndex(int index) {
+    return getPlanetList().get(index);
+  }
+
+  public List<WebElement> getPlanetList() {
+    menuService.openPage(MenuEnum.UBERSICHT);
+
+    return firefox.get().findElements(By.className("smallplanet"));
+  }
+
+  public int countPlanets() {
+    return getPlanetList().size();
+  }
+
+  public void extractFreeFields() {
+    try {
+      List<WebElement> spans = firefox.get().findElement(By.id("diameterContentField")).findElements(By.tagName("span"));
+      if (!spans.isEmpty()) {
+        currentFields = Integer.valueOf(spans.get(0).getText());
+        maxFields = Integer.valueOf(spans.get(1).getText());
+      }
+    } catch (StaleElementReferenceException | NumberFormatException ex) {
+      log.info("extractFreeFields: " + ex.getMessage());
     }
+  }
 
-    public WebElement getPlanetByIndex(int index) {
-        return getPlanetList().get(index);
-    }
+  public boolean hasGrowEnough() {
+    return currentFields > MIN_DEVELOPMENT;
+  }
 
-    public List<WebElement> getPlanetList() {
-        menuService.openPage(MenuEnum.UBERSICHT);
+  public boolean hasPoints() {
+    return points > 0;
+  }
 
-        return firefox.get().findElements(By.className("smallplanet"));
-    }
+  public boolean hasFreeFields() {
+    return getFreeFields() > 1;
+  }
 
-    public int countPlanets() {
-        return getPlanetList().size();
-    }
+  public int getFreeFields() {
+    return maxFields - currentFields;
+  }
 
-    public void extractFreeFields() {
-        try {
-          List<WebElement> spans = firefox.get().findElement(By.id("diameterContentField")).findElements(By.tagName("span"));
-          if (!spans.isEmpty()) {
-            currentFields = Integer.valueOf(spans.get(0).getText());
-            maxFields = Integer.valueOf(spans.get(1).getText());
-          }
-        } catch (StaleElementReferenceException | NumberFormatException ex) {
-          log.info("extractFreeFields: " + ex.getMessage());
-        }
-    }
+  public boolean hasFields() {
+    return currentFields > 0 && maxFields > 0;
+  }
 
-    public boolean hasGrowEnough() {
-        return currentFields > MIN_DEVELOPMENT;
-    }
-
-    public boolean hasPoints() {
-        return points > 0;
-    }
-
-    public boolean hasFreeFields() {
-        return getFreeFields() > 1;
-    }
-
-    public int getFreeFields() {
-        return maxFields - currentFields;
-    }
-
-    public boolean hasFields() {
-        return currentFields > 0 && maxFields > 0;
-    }
-
-    public long getPoints() {
-        return points;
-    }
+  public long getPoints() {
+    return points;
+  }
 }
